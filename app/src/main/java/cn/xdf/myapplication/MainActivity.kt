d6701aab6c2e57032399eeb0e5a6aa599aa2d502
fumanman <fumanman@xdf.cn>
2026-01-10 14:14:14 +0800

    compose入门指引完成

--- app/src/main/java/cn/xdf/myapplication/MainActivity.kt
+++ app/src/main/java/cn/xdf/myapplication/MainActivity.kt
@@ -1,15 +1,27 @@
 package cn.xdf.myapplication
 
-import android.R
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
@@ -18,6 +30,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -27,84 +40,107 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import cn.xdf.myapplication.bean.Message
 import cn.xdf.myapplication.ui.widget.MyApplicationTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.mutableStateOf
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-            var presses by remember { mutableIntStateOf(0) }
-            // 通过 Scaffold 的包装使用组件
             MyApplicationTheme {
-                Scaffold(
-                    // 1、放置顶部栏
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Text("My app")
-                            },
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            )
-                        )
-                    },
-                    // 2、放置底部栏
-                    bottomBar = {
-                        BottomAppBar(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                            contentColor = MaterialTheme.colorScheme.primary
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                textAlign = TextAlign.Center,
-                                text = "Bottom bar"
-                            )
-                        }
-                    },
-                    // 3、放置浮窗操作按钮
-                    floatingActionButton = {
-                        FloatingActionButton(onClick = { presses++ }) {
-                            Icon(Icons.Default.Add, contentDescription = "Add")
-                        }
-                    },
+                Surface(modifier = Modifier.fillMaxSize()) {
 
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Greeting(
-                        name = "Compose  $presses",
-                        modifier = Modifier.padding(innerPadding)
+
+                    Conversation(getMessages())
+//                    MessageCard(Message("Compose", "Hello  I`m Compose"))
+                }
+
+            }
+        }
+    }
+
+    fun getMessages(): List<Message> {
+        val messages = mutableListOf<Message>()
+
+        for (i in 1..20) {
+            val message = Message("Tom", "Hello $i times," +
+                    "添加发发撒的发撒大幅度发撒的发as范德萨大法师地方水岸东方" +
+                    "添加发发撒的发撒大幅度发撒的发as范德萨大法师地方水岸东方" +
+                    "添加发发撒的发撒大幅度发撒的发as范德萨大法师地方水岸东方" +
+                    "添加发发撒的发撒大幅度发撒的发as范德萨大法师地方水岸东方" +
+                    "as地方水岸东方啥大事大法师大法师孙大发SD发")
+            messages.add(message)
+        }
+
+        return messages
+    }
+
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row(
+            modifier = Modifier.padding(all = 8.dp),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)// // size 会同时设置 width 和 height
+                    .clip(CircleShape)
+                    .align(Alignment.CenterVertically)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+//            var isExpanded by remember { mutableSetOf(false) }
+            var isExpanded by remember { mutableStateOf(false) }
+            Column (modifier = Modifier.clickable(onClick = {
+                isExpanded = !isExpanded
+            })){
+                Text(
+                    text = msg.author ?: "tom",
+                    style = MaterialTheme.typography.titleSmall,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 2.dp,
+                    color = Color.Yellow,
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                    Text(
+                        text = msg.body ?: "hello",
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if(isExpanded)Int.MAX_VALUE else 1 ,
+                        style = MaterialTheme.typography.bodyMedium
                     )
                 }
+
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            )
+    fun Conversation(messages: List<Message>) {
+        LazyColumn{
+            items(messages) { message ->
+                MessageCard(message)
+            }
+        }
     }
 
-    // 4. 添加预览
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        Greeting("Android")
-    }
 
 }
---
+++ app/src/main/java/cn/xdf/myapplication/bean/Message.kt
@@ -0,0 +1,9 @@
+package cn.xdf.myapplication.bean
+
+/**
+ * @time: 2026/1/10 10:28
+ * @author : fumm
+ * @des : TODO
+ * @version: v1.0.0
+ */
+data class Message(val author: String? = "",val body: String? = "")
