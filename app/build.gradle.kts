22d6ab41c195b9e2ea0a5fe1e5b88a0ce284ae4a
fmm <fmm_330@163.com>
2026-01-11 11:15:13 +0800

    feat:升级AGP版本8.13.2 升级Gradle 版本8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
-    id("com.android.application") version "7.2.0"
-    id("org.jetbrains.kotlin.android") version "1.9.0"
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "cn.xdf.myapplication"
 
-    compileSdk = 34
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "cn.xdf.myapplication"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -38,13 +41,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    // 和上面的compileOptions 不一样 。。。
-    composeOptions {
-        // 2. 指定 Compose 编译器版本 (需要与你的 Kotlin 版本匹配)
-        // 你当前使用 kotlin-stdlib:1.9.0，建议使用如下版本：
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -52,22 +48,13 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-    // 3. 添加 Compose 核心依赖
-    val composeBom = platform("androidx.compose:compose-bom:2024.02.00") // 使用 BOM 管理版本
-    implementation(composeBom)
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3") // 或者使用 material (Material 2)
-
-    // 这里的 activity-compose 版本需要 1.5.0+，你的 context 中 activity 是 1.5.0，最好升级一下
-    implementation("androidx.activity:activity-compose:1.8.2")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 
 
 }
--- app/src/main/java/cn/xdf/myapplication/MainActivity.kt
+++ app/src/main/java/cn/xdf/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -61,7 +60,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
